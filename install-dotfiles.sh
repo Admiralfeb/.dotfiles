977f4404aeadb6b942670ee4c8df2991557f5b58
Zachary Austin <Zachary@admiralfeb.net>
2021-12-05 20:08:58 -0600

    add install dotfiles script

---
+++ install-dotfiles.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+FILES_TO_REPLACE=(
+    .profile
+    .bashrc
+    .bash_aliases
+)
+
+for file_to_replace in ${FILES_TO_REPLACE[@]}; do
+    [[ -f $HOME/$file_to_replace ]] && mv $HOME/$file_to_replace $HOME/$file_to_replace.backup
+    ln -s $HOME/dotfiles/$file_to_replace $HOME/$file_to_replace
+done
